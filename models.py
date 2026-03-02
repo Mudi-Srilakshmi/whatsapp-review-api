a72f170b99eb553844fe5260f17a933e40426458
Mudi Sri Lakshmi <srilakshmimudi21@gmail.com>
2026-03-02 13:05:41 +0530

    Update models.py

--- models.py
+++ models.py
@@ -2,10 +2,17 @@ from sqlalchemy import Column, Integer, String, DateTime
 from datetime import datetime
 from database import Base
 
+
 class Review(Base):
     __tablename__ = "reviews"
 
     id = Column(Integer, primary_key=True, index=True)
-    phone = Column(String, index=True)
-    message = Column(String)
-    created_at = Column(DateTime, default=datetime.utcnow)
+
+    phone = Column(String(15), nullable=False, index=True)
+
+    message = Column(String(255), nullable=False)
+
+    created_at = Column(DateTime, default=datetime.utcnow, index=True)
+
+    def __repr__(self):
+        return f"<Review id={self.id} phone={self.phone}>"
